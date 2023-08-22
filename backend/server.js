daed63d1113e51bd17e31163da6053a60959e319
markhill343 <markus.hillreiner@hs-augsburg.de>
2023-08-23 00:43:29 +0200

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,75 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const mongoose = require('mongoose');
+const Datastore = require('nedb');
 const cors = require('cors');
-const Canvas = require('./models/Canvas');
-const Event = require('./models/Events');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const canvasDB = new Datastore();
+const eventDB = new Datastore();
+
 app.use(cors()); // To handle CORS issues
 app.use(express.json()); // For parsing JSON request bodies
 
-mongoose.connect('mongodb://localhost:27017/canvasApp', { useNewUrlParser: true, useUnifiedTopology: true });
+// Canvas Routes:
 
-
-//Routes
-// Canvas
 // Create new canvas
-app.post('/api/canvas', async (req, res) => {
-    const canvas = new Canvas();
-    await canvas.save();
-    res.status(201).json(canvas);
+app.post('/api/canvas', (req, res) => {
+    const canvas = {};
+    canvasDB.insert(canvas, (err, newDoc) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+        res.status(201).json(newDoc);
+    });
 });
 
 // Get all canvases
-app.get('/api/canvas', async (req, res) => {
-    const canvases = await Canvas.find().populate('events');
-    res.json(canvases);
-
+app.get('/api/canvas', (req, res) => {
+    canvasDB.find({}, (err, docs) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+        res.json(docs);
+    });
 });
 
 // Get a specific canvas
-app.get('/api/canvas/:canvasId', async (req, res) => {
-    const canvas = await Canvas.findById(req.params.canvasId).populate('events');
-    if (canvas) {
-        res.json(canvas);
-    } else {
-        res.status(404).send({ message: 'Canvas not found!' });
-    }
+app.get('/api/canvas/:canvasId', (req, res) => {
+    const canvasId = req.params.canvasId;
+    canvasDB.findOne({ _id: canvasId }, (err, doc) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+        if (!doc) {
+            res.status(404).send({ message: 'Canvas not found!' });
+            return;
+        }
+        res.json(doc);
+    });
 });
 
-// Websockets Event
+// Event Routes:
+
+// Create a new event
+app.post('/api/event', (req, res) => {
+    const event = req.body;
+    eventDB.insert(event, (err, newDoc) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+        res.status(201).json(newDoc);
+    });
+});
+
+// Websockets Event:
 io.on('connection', (socket) => {
     // Register client with a unique color code
     const clientId = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -53,12 +80,15 @@ io.on('connection', (socket) => {
     });
 
     // Listen for client registration for a specific canvas
-    socket.on('registerForCanvas', async (canvasId) => {
-        const canvas = await Canvas.findById(canvasId).populate('events');
-        if (canvas) {
-            socket.join(canvasId);
-            socket.emit('eventsForCanvas', canvas.events);
-        }
+    socket.on('registerForCanvas', (canvasId) => {
+        canvasDB.findOne({ _id: canvasId }, (err, canvas) => {
+            if (canvas) {
+                socket.join(canvasId);
+                eventDB.find({ canvasId: canvas._id }, (err, events) => {
+                    socket.emit('eventsForCanvas', events);
+                });
+            }
+        });
     });
 
     // Listen for client unregistration from a specific canvas
@@ -67,33 +97,21 @@ io.on('connection', (socket) => {
     });
 
     // Listen for incoming events
-    socket.on('event', async (data) => {
-        const event = new Event(data);
-        await event.save();
-        const canvas = await Canvas.findById(data.canvasId);
-        if (canvas) {
-            canvas.events.push(event);
-            await canvas.save();
-            io.to(data.canvasId).emit('event', event); // Broadcast the event to clients of the specific canvas
-        }
+    socket.on('event', (data) => {
+        const event = data;
+        eventDB.insert(event, (err, newDoc) => {
+            if (err) {
+                console.error("Error inserting event:", err);
+                return;
+            }
+            canvasDB.findOne({ _id: data.canvasId }, (err, canvas) => {
+                if (canvas) {
+                    io.to(data.canvasId).emit('event', newDoc);
+                }
+            });
+        });
     });
 });
-
-// Create a new event
-app.post('/api/event', async (req, res) => {
-    const event = new Event(req.body);
-    await event.save();
-    const canvas = await Canvas.findById(event.canvas);
-    if (canvas) {
-        canvas.events.push(event);
-        await canvas.save();
-    }
-    res.status(201).json(event);
-});
-
-function generateColorCode() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
